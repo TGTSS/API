80ba348da7e35ed7f8cad2c52957a06b3d57bddf
Ulife222 <12824139358@ulife.com.br>
2025-01-25 15:03:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Conexão ao MongoDB
 mongoose
   .connect(
