482d5e72eb038b3f4eb0fd61c67830cb8cf6cb7b
Ulife222 <12824139358@ulife.com.br>
2025-01-29 15:11:22 -0300

    Update ConsultaCNPJ.js

--- components/ConsultaCNPJ.js
+++ components/ConsultaCNPJ.js
@@ -1,7 +1,6 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
-import mongoose from "mongoose";
 import Fornecedor from "../models/Fornecedor.js"; // Importar o modelo Fornecedor
 
 const app = express();
@@ -29,8 +28,38 @@ app.post("/api/fornecedores", async (req, res) => {
 app.get("/consulta/:cnpj", async (req, res) => {
   const cnpj = req.params.cnpj;
   try {
-    const response = await axios.get(`https://api-urh2.onrender.com/consulta/${cnpj}`);
-    res.json(response.data);
+    const response = await axios.get(
+      `https://www.receitaws.com.br/v1/cnpj/${cnpj}`
+    );
+    const {
+      nome,
+      fantasia,
+      cnpj: cnpjRetornado,
+      logradouro,
+      numero,
+      municipio,
+      bairro,
+      uf,
+      cep,
+      email,
+      telefone,
+    } = response.data;
+    const filteredData = {
+      nome,
+      fantasia,
+      cnpj: cnpjRetornado,
+      endereco: {
+        logradouro,
+        numero,
+        municipio,
+        bairro,
+        uf,
+        cep,
+      },
+      email,
+      telefone,
+    };
+    res.json(filteredData);
   } catch (error) {
     console.error("Erro ao consultar o CNPJ:", error.message);
     res.status(500).json({ error: "Erro ao consultar o CNPJ" });
