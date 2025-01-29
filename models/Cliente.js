818f245d1d18e25f79d2779b481bd16bf8d13231
Ulife222 <12824139358@ulife.com.br>
2025-01-29 10:15:44 -0300

    Update Cliente.js

--- models/Cliente.js
+++ models/Cliente.js
@@ -1,37 +1,33 @@
 import mongoose from "mongoose";
 
-const contatoSchema = new mongoose.Schema({
-  nome: String,
-  telefone: String,
-  cargo: String,
-  email: String,
-});
-
 const clienteSchema = new mongoose.Schema({
   tipo: { type: String },
   tambemFornecedor: { type: Boolean },
   status: { type: String },
-  nomeFantasia: String,
-  razaoSocial: String,
-  cnpj: String,
-  inscricaoEstadual: String,
-  inscricaoMunicipal: String,
-  cpf: String,
-  nome: String,
+  nomeFantasia: { type: String },
+  razaoSocial: { type: String },
+  cnpj: { type: String },
+  inscricaoEstadual: { type: String },
+  inscricaoMunicipal: { type: String },
   telefone1: { type: String },
   email: { type: String },
   logradouro: { type: String },
   numero: { type: String },
-  complemento: String,
+  complemento: { type: String },
   bairro: { type: String },
   cidade: { type: String },
   estado: { type: String },
   cep: { type: String },
-  contatos: [contatoSchema],
-  selectedPhone: String,
-  informacoesComplementares: String,
-  documentos: [String],
-  segmento: String,
+  contatos: [
+    {
+      nome: { type: String },
+      telefone: { type: String },
+      cargo: { type: String },
+      email: { type: String },
+    },
+  ],
+  informacoesComplementares: { type: String },
+  documentos: [{ type: String }],
 });
 
 const Cliente = mongoose.model("Cliente", clienteSchema);
