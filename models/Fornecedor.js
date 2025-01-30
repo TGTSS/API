20a45ce57e678a8532e6f78017487471035b6f45
Ulife222 <12824139358@ulife.com.br>
2025-01-30 17:20:16 -0300

    Update

--- models/Fornecedor.js
+++ models/Fornecedor.js
@@ -6,7 +6,7 @@ const FornecedorSchema = new mongoose.Schema({
   status: { type: String },
   nomeFantasia: { type: String },
   razaoSocial: { type: String },
-  cnpj: { type: String },
+  cnpj: { type: String, unique: true },
   inscricaoEstadual: { type: String },
   inscricaoMunicipal: { type: String },
   telefone1: { type: String },
