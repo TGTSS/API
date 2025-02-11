d35407930feafbc0a4ec5da0ec5674370033e874
Ulife222 <12824139358@ulife.com.br>
2025-02-11 09:55:32 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,10 @@ router.post("/register", async (req, res) => {
   try {
     console.log("Rota POST /api/users/register chamada");
     const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email já está em uso" });
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
