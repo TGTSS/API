384b448f830bdc7c8d687c5e66a274c609a2feaa
Ulife222 <12824139358@ulife.com.br>
2025-02-20 09:42:38 -0300

    Obras

--- models/Obra.js
+++ models/Obra.js
@@ -15,6 +15,7 @@ const ObraSchema = new mongoose.Schema({
     ref: "StatusObra",
   },
   codigo: { type: String },
+  codigoObras: { type: String },
   tipo: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "TipoObra",
@@ -55,6 +56,53 @@ const ObraSchema = new mongoose.Schema({
   },
 });
 
+const tipoAbreviacoes = {
+  "Casa térrea": "CT",
+  Sobrado: "SO",
+  Apartamento: "AP",
+  "Condomínio residencial": "CR",
+  "Edifício multifamiliar": "EM",
+  "Prédio de alto padrão": "PP",
+  "Habitação popular": "HP",
+  "Construção própria": "CP",
+  "Lojas e estabelecimentos comerciais": "LC",
+  "Shopping centers": "SC",
+  "Supermercados e atacadistas": "SA",
+  "Escritórios e centros empresariais": "EE",
+  "Hotéis e pousadas": "HP",
+  "Restaurantes e bares": "RB",
+  Fábricas: "FA",
+  "Galpões industriais": "GI",
+  "Armazéns e depósitos": "AD",
+  "Centros de distribuição": "CD",
+  "Instalações metalúrgicas": "IM",
+  "Refinarias de petróleo e gás": "RP",
+  Rodoviária: "RO",
+  Ferroviária: "FE",
+  Aeroportuária: "AE",
+  "Portuária e marítima": "PM",
+  Saneamento: "SA",
+  Hidráulica: "HI",
+  Energética: "EN",
+  Telecomunicações: "TC",
+  "Reforma residencial": "RR",
+  "Reforma comercial": "RC",
+  "Restauração de patrimônio histórico": "RH",
+  "Revitalização urbana": "RU",
+  "Reforço estrutural": "RE",
+  Impermeabilização: "IM",
+  "Manutenção predial": "MP",
+  "Troca de fachadas e revestimentos": "TF",
+  "Topografia e geotecnia": "TG",
+  Demolição: "DE",
+  Pavimentação: "PA",
+  "Obras subterrâneas": "OS",
+  Paisagismo: "PV",
+  "Obras de acessibilidade": "OA",
+  "Obras acústicas": "OC",
+  "Iluminação pública": "IP",
+};
+
 ObraSchema.pre("save", async function (next) {
   if (this.isNew) {
     const counter = await Counter.findByIdAndUpdate(
@@ -62,7 +110,10 @@ ObraSchema.pre("save", async function (next) {
       { $inc: { seq: 1 } },
       { new: true, upsert: true }
     );
-    this.codigo = counter.seq.toString().padStart(6, "0");
+    const tipoAbreviacao = tipoAbreviacoes[this.tipo] || "OT"; // OT para "Outros"
+    this.codigo = `${tipoAbreviacao}-${counter.seq
+      .toString()
+      .padStart(2, "0")}`;
   }
   next();
 });
