a649d70f41b7b6295ebf58224d23f06d657f9329
Ulife222 <12824139358@ulife.com.br>
2025-03-19 08:23:37 -0300

    Update Composicao.js

--- models/Composicao.js
+++ models/Composicao.js
@@ -8,10 +8,19 @@ const ComposicaoSchema = new mongoose.Schema({
   custoTotal: { type: Number },
 });
 
-// Middleware para converter custoTotal de string para número
+// Middleware para converter custoTotal de string para número antes de salvar
 ComposicaoSchema.pre("save", function (next) {
   if (typeof this.custoTotal === "string") {
     this.custoTotal = parseFloat(this.custoTotal.replace(",", "."));
+  }
+  next();
+});
+
+// Middleware para converter custoTotal de string para número antes de atualizar
+ComposicaoSchema.pre("findOneAndUpdate", function (next) {
+  const update = this.getUpdate();
+  if (update.custoTotal && typeof update.custoTotal === "string") {
+    update.custoTotal = parseFloat(update.custoTotal.replace(",", "."));
   }
   next();
 });
