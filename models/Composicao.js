f41f84cfd5a1640cd93dca70fc053feb5f7aca74
Ulife222 <12824139358@ulife.com.br>
2025-03-19 08:29:07 -0300

    Composições

--- models/Composicao.js
+++ models/Composicao.js
@@ -5,25 +5,9 @@ const ComposicaoSchema = new mongoose.Schema({
   codigoComposicao: { type: Number },
   descricaoComposicao: { type: String },
   unidade: { type: String },
-  custoTotal: { type: Number },
+  custoTotal: { type: String },
 });
 
-// Middleware para converter custoTotal de string para número antes de salvar
-ComposicaoSchema.pre("save", function (next) {
-  if (typeof this.custoTotal === "string") {
-    this.custoTotal = parseFloat(this.custoTotal.replace(",", "."));
-  }
-  next();
-});
-
-// Middleware para converter custoTotal de string para número antes de atualizar
-ComposicaoSchema.pre("findOneAndUpdate", function (next) {
-  const update = this.getUpdate();
-  if (update.custoTotal && typeof update.custoTotal === "string") {
-    update.custoTotal = parseFloat(update.custoTotal.replace(",", "."));
-  }
-  next();
-});
 
 const Composicao = mongoose.model("Composicao", ComposicaoSchema);
 
