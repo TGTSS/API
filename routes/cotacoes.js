b27e3649f542520cad76e3ca4c22d90cd0221b21
Ulife222 <12824139358@ulife.com.br>
2025-03-26 09:24:06 -0300

    Update cotacoes.js

--- routes/cotacoes.js
+++ routes/cotacoes.js
@@ -359,10 +359,12 @@ router.patch("/:cotacaoId", async (req, res) => {
     await cotacao.save();
     res.status(200).json(cotacao);
   } catch (error) {
-    res.status(500).json({
-      message: "Erro ao atualizar status da cotação",
-      error: error.message,
-    });
+    res
+      .status(500)
+      .json({
+        message: "Erro ao atualizar status da cotação",
+        error: error.message,
+      });
   }
 });
 
