c0547cdc462a9216b1a6c2ba3dfa347d2b8e64a1
Ulife222 <12824139358@ulife.com.br>
2025-07-29 10:43:17 -0300

    Update Medicao.js

--- models/Medicao.js
+++ models/Medicao.js
@@ -24,7 +24,7 @@ const medicaoSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["Aprovação", "Em andamento", "Concluído"],
+    enum: ["Aprovação", "Em andamento", "Concluído, Pendente"],
     default: "Aprovação",
   },
   groups: [
